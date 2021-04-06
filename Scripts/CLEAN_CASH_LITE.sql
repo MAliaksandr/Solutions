1875361d77537533411b8bfe105466a52d09daab
m.aliaksandr <mosya911@gmail.com>
2021-04-06 11:58:31 +0300

    Запрос очистки кассы. Lite.

---
+++ Scripts/CLEAN_CASH_LITE.sql
@@ -0,0 +1,26 @@
+EXECUTE BLOCK(
+ADATE DATE =:ADATE
+)
+as
+BEGIN
+  DELETE FROM USR$PF_CHECKLINE WHERE USR$DATE<=:ADATE;
+  DELETE FROM USR$PF_CHECK WHERE USR$DATE<=:ADATE;
+  /*ËÎÃ ÄÅÉÑÒÂÈÉ ÊÀÑÑÈÐÀ*/
+  DELETE
+  FROM USR$PF_ACTIONLOG A
+   WHERE
+  a.EDITIONDATE<=:adate
+  and
+  not exists
+  ( SELECT
+      f.ID
+    FROM USR$PF_CHECK f
+    where f.ID = a.USR$CHECKKEY );
+  /*ËÎÃ ÎÏÅÐÀÖÈÉ ÔÐ*/
+  DELETE
+  FROM USR$PF_LOGOPERATION l
+  WHERE l.USR$DATE<=:ADATE;
+END
+
+
+
